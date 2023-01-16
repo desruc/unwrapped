01c807eab465fcef76cbcc122a3801e924a9b11f
James Cameron <36631337+desruc@users.noreply.github.com>
2023-01-16 22:08:27 +1000

    feat: add tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import AuthContext from "@/components/auth/AuthContext";
-import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import "@/styles/globals.css";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       {/*
