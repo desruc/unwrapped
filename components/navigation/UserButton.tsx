fa4ebfc4d081e43cf9982898f218a7c1d85a6c63
James Cameron <36631337+desruc@users.noreply.github.com>
2023-01-20 08:58:26 +1000

    feat: styles for user button

--- components/navigation/UserButton.tsx
+++ components/navigation/UserButton.tsx
@@ -21,10 +21,22 @@ async function UserButtonInner() {
   const profile = await getProfile();
 
   const image = profile?.images?.[0]?.url ?? "placeholder image";
+  const name = profile?.display_name;
 
   return (
-    <div className="relative w-[60px] h-[60px]">
-      <Image src={image} alt="Profile image" fill className="rounded-[50%]" />
+    <div className="grid grid-cols-[auto_1fr] gap-3 w-full p-2 rounded-lg cursor-pointer transition-all hover:bg-gray-900 hover:shadow-md">
+      <div className="flex items-center">
+        <div className="relative w-[50px] h-[50px]">
+          <Image src={image} alt="Profile image" fill className="rounded-[50%]" />
+        </div>
+      </div>
+      {name && (
+        <div className="flex items-center">
+          <span className="select-none block text-sm font-semibold tracking-wide">
+            {name}
+          </span>
+        </div>
+      )}
     </div>
   );
 }
