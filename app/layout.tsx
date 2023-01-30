90a4ef40b9601dde7337d3620c6ed52bb68bd501
James Cameron <jdonaldcameron@gmail.com>
2023-01-31 07:28:48 +1000

    feat: add font (#6)
    * feat: add gotham font
    
    * refactor: swap to figtree font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,15 @@
 import AuthContext from "@/components/auth/AuthContext";
+import { Figtree } from "@next/font/google";
+
+const figtree = Figtree({
+  variable: "--font-figtree"
+});
 
 import "@/styles/globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={figtree.variable}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
