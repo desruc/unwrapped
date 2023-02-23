02d2fcc9eef292a8cac89cae31e2697f51d55985
James Cameron <36631337+desruc@users.noreply.github.com>
2023-02-23 22:28:36 +1000

    chore: update favicon

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,12 @@
 export default function Head() {
   return (
     <>
-      <title>Unwrapped</title>
+      <title>unwrapped.</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="This is unwrapped" />
+      <meta
+        name="description"
+        content="unwrapped is a web app that utilizes the Spotify API to provide a visual representation of your personalized data."
+      />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
