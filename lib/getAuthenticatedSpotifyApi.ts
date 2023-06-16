b938565abc79414507a6f5dbea1dab3bffe7d3e0
James Cameron <jdonaldcameron@gmail.com>
2023-06-16 23:12:57 +1000

    chore: update deps (#10)

--- lib/getAuthenticatedSpotifyApi.ts
+++ lib/getAuthenticatedSpotifyApi.ts
@@ -1,11 +1,11 @@
-import { unstable_getServerSession } from "next-auth";
+import { getServerSession } from "next-auth";
 import { authOptions } from "./auth";
 import { spotifyApi } from "./spotify";
 
 export type TimeRange = "short" | "medium" | "long";
 
 export async function getAuthenticatedSpotifyApi() {
-  const session = await unstable_getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
   if (session?.user.accessToken) {
     spotifyApi.setAccessToken(session.user.accessToken);
